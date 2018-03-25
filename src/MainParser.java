e958304d6056fb9ef7f6cbedc26e313b76ce7263
JeanDuarte00 <jean_luccas320@hotmail.com>
2018-03-25 19:37:36 -0300

    usando outro MAIN para testar com o parser

---
+++ src/MainParser.java
@@ -0,0 +1,18 @@
+
+
+import parser.Parser;
+
+public class MainParser {
+
+	public static void main(String[] args) {
+		
+		Parser parser = new Parser(args[0]); // cria o analisador lexico
+		
+		while(true) {			
+					
+			parser.execute();
+			break;	
+		}
+		
+	}
+}
