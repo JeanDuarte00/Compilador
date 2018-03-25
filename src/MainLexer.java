53402df88e2b86ca625c13dd338df9eb85ac8dbc
JeanDuarte00 <jean_luccas320@hotmail.com>
2018-03-25 19:38:14 -0300

    Alterando nome para testar só o lexer

---
+++ src/MainLexer.java
@@ -0,0 +1,27 @@
+import lexer.Lexer;
+import lexer.Token;
+import lexer.TokensId;
+
+
+public class MainLexer {
+
+	static Token token;
+	
+	public static void main(String args[]) throws Exception {
+		
+		Lexer lexer = new Lexer(args[0]); // cria o analisador lexico
+		
+		while(true) {			
+					
+			token = lexer.getNextToken();
+			
+			System.out.println("tokenID: "+token.getId() +" | "+ "lexema: " + token.getLexema());				
+			
+			
+			if(token.getId() == TokensId.ENDFILE.getId()) {	
+				break;
+			}
+		}
+		
+	}
+}
